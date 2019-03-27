6f2730ac25510b3e0ef0ee32bd44faef85f99c2b
RazersGaming <49001773+RazersGaming@users.noreply.github.com>
2019-03-27 19:47:18 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("Pub, *Prefix");
+    console.log("Le bot a bien ete connecter");
+});
+
+bot.login("NTYwNTA3Njc4MDExNDI0NzY5.D31BKQ.uwxaKOiQf1kmiOQmCcLMtWsZP-I");
