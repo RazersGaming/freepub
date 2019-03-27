2ff48ba6df58690c504e27aa2c9aa9d070250702
RazersGaming <49001773+RazersGaming@users.noreply.github.com>
2019-03-27 20:11:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,11 +8,15 @@ bot.on("ready", function() {
 });
 
 bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
     if (msg.content === prefix + "site"){
         msg.channel.send("https://http://freepub.web.boxtoplay.com/lexpgm.000webhostapp.com/")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
-
-});
 
 bot.login("NTYwNTA3Njc4MDExNDI0NzY5.D31BKQ.uwxaKOiQf1kmiOQmCcLMtWsZP-I");
