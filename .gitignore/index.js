4c95a55330735fada893cdb0d32a3149571c8ce2
RazersGaming <49001773+RazersGaming@users.noreply.github.com>
2019-03-27 20:27:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,24 +1,18 @@
 const Discord = require("discord.js");
 
-var bot = new Discord.Client();
+const Help = require('./command/help')
 
-const prefix = ("*");
+var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("Pub, *Prefix");
     console.log("Le bot a bien ete connecter");
 });
 
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://http://freepub.web.boxtoplay.com/lexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
+bot.on('message', message => {
+
+    Help.parse(message)
+
+})
 
 bot.login("NTYwNTA3Njc4MDExNDI0NzY5.D31BKQ.uwxaKOiQf1kmiOQmCcLMtWsZP-I");
