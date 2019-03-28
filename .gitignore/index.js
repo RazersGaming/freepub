181f0236f31245f0160412938906360a81e6455e
RazersGaming <49001773+RazersGaming@users.noreply.github.com>
2019-03-28 07:09:07 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,20 +1,10 @@
-const client = new Discord.Client();
- 
-client.on("ready", () => {
-  console.log("I am ready!");
-});
- 
-const prefix = "p!";
-client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
- 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("Pub, *Prefix");
+    console.log("Le bot a bien ete connecter");
 });
 
 bot.login("NTYwNTA3Njc4MDExNDI0NzY5.D31BKQ.uwxaKOiQf1kmiOQmCcLMtWsZP-I");
