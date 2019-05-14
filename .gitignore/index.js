7649393cb0b5a83c60aa6794335ba73ca31a9cd9
RazersGaming <49001773+RazersGaming@users.noreply.github.com>
2019-05-14 18:21:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,42 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+const token = process.env.token // a garder en version heroku
+const prefix = ("?");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+bot.on('ready', function () {
+    console.log("Je suis prêt à être utilisé.")
+    bot.user.setActivity('rien').catch(console.error)
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        console.log(`${member.displayName} à rejoind le serveur.`)
+    }).catch(console.error)
+});
+
+const ban = require('./kick et ban/ban');
+require('./embed/RichEmbed');
+
+bot.on('message', function (message){
+    if (ban.match(message)){
+        return ban.action(message)
+    }
+});
+
+
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "site"){
+        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        console.log("Une personne a demandé pour aller sur ton site.")
+    }
+
 });
 
 client.login('NTc3ODkwMjYxODg4NDAxNDM5.XNrqOA.ToK1418VRPbyb7uLGFMkBDWudKw');
